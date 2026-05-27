e203adba1a311f7462c112a8e60d25819b98e226
agadadesmond69-dot <agadadesmond69@gmail.com>
2026-05-27 06:25:34 +0100

    Add files via upload

---
+++ youtube_sql_queries.sql
@@ -0,0 +1,11 @@
+Queries
+1. Which video categories have the highest average views, and engagement rate
+2. Which video categories showed the highest year-over-year growth in views
+3. Which categories consistently underperform in views and engagement across years?
+4. Calculate YoY growth rate of views
+5. What publishing days trend fastest?
+6. Analyze correlation between video duration and performance matrics (views)
+7. Which channels contribute disproportionately to total views and engagement?
+8. Which countries have the highest engagement rate relative to total views?
+9. Does higher view count strogly correlate with higher engagement or are diminishing returns?
+10. Year-over-year Growth Trend Analysis of Video Categories
